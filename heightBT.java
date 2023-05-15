338d696e633380f8989b2ff0d53be146ed06f095
itsmaanish <kmanishraj5@gmail.com>
2023-05-15 21:48:20 +0530

    Height of a tree

---
+++ heightBT.java
@@ -0,0 +1,13 @@
+class Solution {
+    //Function to find the height of a binary tree.
+    int height(Node node) 
+    {
+        //base case
+        if(node==null){
+            return 0;
+        }
+        int lh= height(node.left);
+        int rh= height(node.right);
+        return  Math.max(lh,rh)+1;
+    }
+}
