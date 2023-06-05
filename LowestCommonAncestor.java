f529c125d59de5a8ee25d38aaa0dc24a677bf972
itsmaanish <kmanishraj5@gmail.com>
2023-06-05 18:27:09 +0530

    Lowest Common Ancestor in a BST

---
+++ LowestCommonAncestor.java
@@ -0,0 +1,18 @@
+
+class BST
+{   
+	Node LCA(Node root, int n1, int n2)
+	{   if(root == null){
+	    return null;
+	}
+        if(n1 < root.data && n2 < root.data){
+            return LCA(root.left, n1, n2);
+        }
+        else if(n1 > root.data && n2 > root.data){
+            return LCA(root.right, n1, n2);
+        }else{
+            return root;
+        }
+    }
+    
+}
